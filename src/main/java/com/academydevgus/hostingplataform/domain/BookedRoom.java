365311debc35a1864f14aeb0521acc1a58fc3293
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-04-23 19:36:42 -0300

    feat(bookedRoom): adding attributes
    Applying all the necessary attributes to the BookedRoom class with the Jakarta and JPA annotations. Also editing the properties to yml and configuring the MySQL database.

--- src/main/java/com/academydevgus/hostingplataform/domain/BookedRoom.java
+++ src/main/java/com/academydevgus/hostingplataform/domain/BookedRoom.java
@@ -1,4 +1,133 @@
 package com.academydevgus.hostingplataform.domain;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+    @Column(name = "guest_fullname")
+    private String guestFullName;
+    @Column(name = "guest_email")
+    private String guestEmail;
+    @Column(name = "adults")
+    private int numOfAdults;
+    @Column(name = "children")
+    private int numOfChildren;
+    @Column(name = "total_guest")
+    private int totalNumOfGuest;
+    @Column(name = "booking_confirmation_code")
+    private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumOfGuest() {
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+    }
+
+    public BookedRoom() {
+    }
+
+    public BookedRoom(Long bookingId, LocalDate checkInDate,
+                      LocalDate checkOutDate, String guestFullName, String guestEmail, int numOfAdults,
+                      int numOfChildren, int totalNumOfGuest, String bookingConfirmationCode, Room room) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.guestFullName = guestFullName;
+        this.guestEmail = guestEmail;
+        this.numOfAdults = numOfAdults;
+        this.numOfChildren = numOfChildren;
+        this.totalNumOfGuest = totalNumOfGuest;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+        this.room = room;
+    }
+
+    public int getNumOfAdults() {
+        return numOfAdults;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumOfGuest();
+    }
+
+    public int getNumOfChildren() {
+        return numOfChildren;
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumOfGuest();
+    }
+    public String getBookingConfirmationCode() {
+        return bookingConfirmationCode;
+    }
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getGuestFullName() {
+        return guestFullName;
+    }
+
+    public void setGuestFullName(String guestFullName) {
+        this.guestFullName = guestFullName;
+    }
+
+    public String getGuestEmail() {
+        return guestEmail;
+    }
+
+    public void setGuestEmail(String guestEmail) {
+        this.guestEmail = guestEmail;
+    }
+
+    public int getTotalNumOfGuest() {
+        return totalNumOfGuest;
+    }
+
+    public void setTotalNumOfGuest(int totalNumOfGuest) {
+        this.totalNumOfGuest = totalNumOfGuest;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 }
