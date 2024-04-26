6efa6bc95586b93329d59448528a5e10d222f357
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-04-25 22:46:33 -0300

    feat(room): service method to add room
    Adding the method from service that can create new room.

--- src/main/java/com/academydevgus/hostingplataform/service/ImpRoomService.java
+++ src/main/java/com/academydevgus/hostingplataform/service/ImpRoomService.java
@@ -1,4 +1,40 @@
 package com.academydevgus.hostingplataform.service;
 
-public class ImpRoomService {
+import com.academydevgus.hostingplataform.domain.Room;
+import com.academydevgus.hostingplataform.repository.RoomRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+@Service
+public class ImpRoomService implements RoomService {
+
+    private RoomRepository roomRepository;
+
+    public ImpRoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public ImpRoomService() {
+    }
+
+    @Override
+    public Room addNewRoom(MultipartFile photoFile, String roomType,
+                           BigDecimal roomPrice)
+            throws SQLException, IOException {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if (!photoFile.isEmpty()) {
+            byte[] photoBytes = photoFile.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setPhoto(photoBlob);
+        }
+        return roomRepository.save(room);
+    }
 }
--- src/main/java/com/academydevgus/hostingplataform/service/RoomService.java
+++ src/main/java/com/academydevgus/hostingplataform/service/RoomService.java
@@ -1,4 +1,12 @@
 package com.academydevgus.hostingplataform.service;
 
+import com.academydevgus.hostingplataform.domain.Room;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
 public interface RoomService {
+    Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws SQLException, IOException;
 }
